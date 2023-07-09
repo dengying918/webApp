b5ece65f9cf20d2e6d9c8f700ee6c6e6a138a3c0
dengying <dengying918@126.com>
2023-07-09 14:05:18 +0800

    fix:更改首页并发布到vercel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63dab64f23cc2aec"; }
+/******/ 		__webpack_require__.h = function() { return "186cebb89b6c04bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
